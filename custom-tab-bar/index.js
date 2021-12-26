4052a58ee6b4087eaac3b475312749e3a8c3dac3
unknown <unknown@example.com>
2021-12-26 22:43:02 +0800

    Initial Commit

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,39 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#9382da",
+    list:[
+      {
+        "pagePath":"../index/index",
+        "iconPath": "../assets/tabbar/home/home.png",
+        "selectedIconPath": "../assets/tabbar/home/home_selected.png",
+        "text": "首页"
+      },
+      {
+        "pagePath": "../logs/logs",
+        "iconPath": "../assets/tabbar/record/record.png",
+        "selectedIconPath": "../assets/tabbar/record/record_selected.png",
+        "text": "记录"
+      },
+      {
+        "pagePath": "../profile/profile",
+        "iconPath": "../assets/tabbar/profile/profile.png",
+        "selectedIconPath": "../assets/tabbar/profile/profile_selected.png",
+        "text": "我的"
+      }
+    ]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
---
+++ pages/logs/logs.js
@@ -0,0 +1,27 @@
+// logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  // 页面显示触发
+  onShow(){
+    if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 1
+        })
+      }
+  },
+  onLoad() {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return {
+          date: util.formatTime(new Date(log)),
+          timeStamp: log
+        }
+      })
+    })
+  }
+})
