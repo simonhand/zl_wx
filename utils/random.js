63a05b2a5492b2f216e545fd53f308b3869afdc3
zhangle <1029838709@qq.com>
2022-01-29 19:45:16 +0800

    口算模块初步实现

--- utils/random.js
+++ utils/random.js
@@ -1,11 +1,17 @@
 
 // 根据qty来决定return几位以内的随机数
-
 export const randomNum = (qty) => {
     const baseNum = Math.random().toFixed(qty);
     console.log("baseNum",baseNum);
     console.log( baseNum * 10 ** qty)
     // 这里想向下取整避免0.1+0.3的情况，反正都是随机数不影响最终结果
-    return Math.floor(baseNum * 10 ** qty) ;
+    return Math.floor(baseNum * 10 ** qty) ;  
+}
+
+// 这个是在 floor 与 ceil之间取随机数（包括floor和ceil）
+export const randomNumBetween = (floor,ceil) => {
+    const baseNum = Math.random();
+    const section = ( ceil - floor) * baseNum;
+    return Math.round(section + floor);
 }
 
