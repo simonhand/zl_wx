7952cc370d5fd5a9677b581179684eb312654c02
zhangle <1029838709@qq.com>
2022-04-10 22:24:38 +0800

    小测也作答done

--- pages/createExam/service.js
+++ pages/createExam/service.js
@@ -1,12 +1,12 @@
 import {
     zlrequest
 } from '../../utils/zlGraphql'
-
+import { zlEncodeList } from '../../utils/util'
 export const createExerciseRequset = (obj) => {
     // 我愿称这为graphql的天坑！！！
     const playLoad = JSON.stringify({
         query: `
-        query createExercise{  createExercise(createrAvatarUrl:"${obj.createrAvatarUrl}",createrId:"${obj.createrId}",course_id:"${obj.course_id}",courseName:"${obj.courseName}",teacherName:"${obj.teacherName}",invitationCode:"${obj.invitationCode}",exerciseList:"${JSON.stringify(obj.exerciseList).replace(/\"/g,"+z&l+")}")
+        query createExercise{  createExercise(createrAvatarUrl:"${obj.createrAvatarUrl}",createrId:"${obj.createrId}",course_id:"${obj.course_id}",courseName:"${obj.courseName}",teacherName:"${obj.teacherName}",invitationCode:"${obj.invitationCode}",exerciseList:"${zlEncodeList(obj.exerciseList)}")
         {createrAvatarUrl,createrId,course_id,courseName,teacherName,invitationCode,}}       
         `
     })
