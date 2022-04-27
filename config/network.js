6e0c3ab891cb65e0e0598b6776599150cb88c739
zhangle <1029838709@qq.com>
2022-04-27 20:07:35 +0800

    record notify done

--- config/network.js
+++ config/network.js
@@ -1,4 +1,5 @@
-const baseUrl = 'http://192.168.0.3:3000/graphql'
+// const baseUrl = 'http://192.168.0.3:3000/graphql'
+const baseUrl = 'http://192.168.1.103:3000/graphql'
 // const baseUrl = 'http://loaclhost:3000/graphql'
 
 // const baseUrl = 'http://192.168.138.73:3000/graphql'
