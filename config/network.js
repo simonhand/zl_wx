079e6fb73343c2a4f15ad7e2b23abf59bc9bcf73
zhangle <1029838709@qq.com>
2022-05-02 22:45:16 +0800

    all need done

--- config/network.js
+++ config/network.js
@@ -1,7 +1,7 @@
-const baseUrl = 'http://192.168.0.3:3000/graphql'
+const baseUrl = 'http://127.0.0.1:3000/graphql'
+// const baseUrl = 'http://192.168.0.3:3000/graphql'
 // const baseUrl = 'http://192.168.1.103:3000/graphql'
 // const baseUrl = 'http://loaclhost:3000/graphql'
-
 // const baseUrl = 'http://192.168.138.73:3000/graphql'
 
 module.exports = {
